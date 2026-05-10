16fc66f92cf7e4e6ac37083a841a63785f114f40
Gabrieusl <128613557+Gabrieusl@users.noreply.github.com>
2026-05-10 09:36:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const grade = [
+
+  ["Seg", "1ª série", "Matemática"],
+  ["Seg", "1ª série", "Matemática"],
+  ["Seg", "1ª série", "Ed. Digital"],
+  ["Seg", "2ª série", "Ed. Financeira"],
+  ["Seg", "2ª série", "Ed. Financeira"],
+  ["Seg", "7º ano", "Matemática"],
+  ["Seg", "7º ano", "Matemática"],
+  ["Seg", "6º ano", "Matemática"],
+  ["Seg", "8º ano", "Matemática"],
+  ["Seg", "8º ano", "Matemática"],
+
+  ["Ter", "3ª série", "Ed. Financeira"],
+
+  ["Qua", "9º ano", "Recomposição"],
+  ["Qua", "9º ano", "Recomposição"],
+  ["Qua", "1ª série", "Matemática"],
+  ["Qua", "1ª série", "Matemática"],
+  ["Qua", "7º ano", "Matemática"],
+  ["Qua", "7º ano", "Matemática"],
+  ["Qua", "6º ano", "Recomposição"],
+  ["Qua", "6º ano", "Recomposição"],
+
+  ["Qui", "6º ano", "Matemática"],
+
+  ["Sex", "3ª série", "Ed. Financeira"],
+  ["Sex", "1ª série", "Ed. Financeira"],
+  ["Sex", "1ª série", "Ed. Financeira"],
+  ["Sex", "1ª série", "Ed. Digital"],
+  ["Sex", "7º ano", "Matemática"],
+  ["Sex", "6º ano", "Matemática"],
+  ["Sex", "6º ano", "Matemática"],
+  ["Sex", "8º ano", "Matemática"],
+  ["Sex", "8º ano", "Matemática"]
+
+];
+
+console.log("Sistema iniciado");
